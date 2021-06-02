f7ac37dd54ef95dd182e6e00c0929db4f94ebd2e
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-02 15:52:35 +0530

    resizing the image

--- img_server/main/views.py
+++ img_server/main/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from wsgiref.util import FileWrapper
 from img_server.settings import BASE_DIR
+from PIL import Image
 import base64
 import os
 
@@ -22,6 +23,9 @@ class PNGRenderer(renderers.BaseRenderer):
 @api_view(["GET", ])
 @renderer_classes([PNGRenderer])
 def index(request):
-    img = open(os.path.join(BASE_DIR, "main/images/map.png"), "rb")
+    img = Image.open(os.path.join(BASE_DIR, "main/images/map.png"))
+    resized_img = img.resize((256, 256))
+    resized_img.save(os.path.join(BASE_DIR, "main/images/map.png"))
     
+    img = open(os.path.join(BASE_DIR, "main/images/map.png"), "rb")
     return Response(FileWrapper(img), status = status.HTTP_200_OK)
