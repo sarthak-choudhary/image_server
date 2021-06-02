0dbeee20e28b0a96cfcd3eb86f6e9d29a4d7cd90
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-02 16:54:29 +0530

    adding httpResponse

--- img_server/main/views.py
+++ img_server/main/views.py
@@ -1,31 +1,17 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.response import Response
-from rest_framework import status
-from wsgiref.util import FileWrapper
+from django.http import HttpResponse
 from img_server.settings import BASE_DIR
 from PIL import Image
-import base64
 import os
 
-from rest_framework import renderers
-
-class PNGRenderer(renderers.BaseRenderer):
-    media_type = 'image/png'
-    format = 'png'
-    charset = None
-    render_style = 'binary'
-
-    def render(self, data, media_type=None, renderer_context=None):
-        return data
-
-# Create your views here.
-@api_view(["GET", ])
-@renderer_classes([PNGRenderer])
 def index(request):
-    img = Image.open(os.path.join(BASE_DIR, "main/images/map.png"))
-    resized_img = img.resize((256, 256))
-    resized_img.save(os.path.join(BASE_DIR, "main/images/map.png"))
-    
-    img = open(os.path.join(BASE_DIR, "main/images/map.png"), "rb")
-    return Response(FileWrapper(img), status = status.HTTP_200_OK)
+    # img = open(os.path.join(BASE_DIR, "main/images/map.png"), "rb")
+    # return Response(FileWrapper(img), content_type="image/jpeg",  status = status.HTTP_200_OK)
+    try:
+        with open(os.path.join(BASE_DIR, "main/images/map.png"), "rb") as f:
+            return HttpResponse(f.read(), content_type="image/jpeg")
+    except IOError:
+        red = Image.new('RGBA', (1, 1), (255,0,0,0))
+        response = HttpResponse(content_type="image/jpeg")
+        red.save(response, "JPEG")
+        return response
