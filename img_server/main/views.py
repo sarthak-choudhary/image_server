b8ff633c89d570ba086858d5241a15171dbced52
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-09 18:51:03 +0530

    centre relocated

--- img_server/main/views.py
+++ img_server/main/views.py
@@ -5,6 +5,8 @@ from PIL import Image
 from pyproj import Proj, transform
 from osgeo import gdal
 import os
+# 76.86674473142777 29.35635551477078 78.88672199967831 28.124043591774523
+# 78.75000000000001 27.05912578437406 81.56250000000001 24.527134822597805
 
 def index(request):
     BBOX = request.GET.get('bbox', 'BBOX')
@@ -15,7 +17,8 @@ def index(request):
     request_bbox = [float(t) for t in BBOX.split(',')]
     request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
     request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
-    out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin=[request_bbox[0], request_bbox[3],request_bbox[2], request_bbox[1]]) 
+    print(request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1])
+    out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]]) 
 
     out_ds = out.ReadAsArray()[0,...]
     img = Image.fromarray(out_ds).resize((256, 256), Image.NEAREST)
