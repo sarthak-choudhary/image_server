81db0067437b6e34ddc29c322cd4280d767dba02
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-09 15:01:35 +0530

    transformation added

--- img_server/main/views.py
+++ img_server/main/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from img_server.settings import BASE_DIR
 from PIL import Image
+from pyproj import Proj, transform
+from osgeo import gdal
 import os
 
 def index(request):
-    # img = open(os.path.join(BASE_DIR, "main/images/map.png"), "rb")
-    # return Response(FileWrapper(img), content_type="image/jpeg",  status = status.HTTP_200_OK)
-    try:
-        with open(os.path.join(BASE_DIR, "main/images/map.png"), "rb") as f:
-            return HttpResponse(f.read(), content_type="image/jpeg")
-    except IOError:
-        red = Image.new('RGBA', (1, 1), (255,0,0,0))
-        response = HttpResponse(content_type="image/jpeg")
-        red.save(response, "JPEG")
-        return response
+    BBOX = request.GET.get('bbox', 'BBOX')
+    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/sat.tif"))
+    inProj = Proj(init = 'epsg:3857')
+    outProj = Proj(init = 'epsg:4326')
+
+    request_bbox = [float(t) for t in BBOX.split(',')]
+    request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
+    request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
+    out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin=[request_bbox[0], request_bbox[3],request_bbox[2], request_bbox[1]]) 
+
+    out_ds = out.ReadAsArray()[0,...]
+    img = Image.fromarray(out_ds).resize((256, 256), Image.NEAREST)
+    response = HttpResponse(content_type="image/png")
+    img.save(response, "PNG")
+    return response
+
