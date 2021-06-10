b0a50d717b026e2aa17b9441697f104e58912b0c
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-10 13:12:57 +0530

    requirements updated

--- source.js
+++ source.js
@@ -1,6 +1,6 @@
-var mymap = L.map("map").setView([25.505, 92.09], 7);
+var mymap = L.map("map").setView([28.61, 77.2090], 7);
 var layer = L.tileLayer.wms("http://127.0.0.1:8000/",{
-	
+	format: "image/png"
 });
 
 layer.addTo(mymap);
