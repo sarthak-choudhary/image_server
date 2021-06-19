40f7e2a0fbc7d0ab916cab96fd2ad806e3a16169
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-19 11:56:56 +0530

    multispectral images added

--- img_server/main/views.py
+++ img_server/main/views.py
@@ -5,12 +5,14 @@ from PIL import Image
 from pyproj import Proj, transform
 from osgeo import gdal
 import os
+import cv2
+import numpy
 # 76.86674473142777 29.35635551477078 78.88672199967831 28.124043591774523
 # 78.75000000000001 27.05912578437406 81.56250000000001 24.527134822597805
 
 def index(request):
     BBOX = request.GET.get('bbox', 'BBOX')
-    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/sat.tif"))
+    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/newsat.tif"))
     inProj = Proj(init = 'epsg:3857')
     outProj = Proj(init = 'epsg:4326')
 
@@ -18,11 +20,24 @@ def index(request):
     request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
     request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
     print(request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1])
+    print()
     out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]]) 
 
-    out_ds = out.ReadAsArray()[0,...]
-    img = Image.fromarray(out_ds).resize((256, 256), Image.NEAREST)
-    response = HttpResponse(content_type="image/png")
-    img.save(response, "PNG")
+    # out_ds = out.ReadAsArray()[0,...]
+    # img = Image.fromarray(out_ds).resize((256, 256), Image.NEAREST)
+    # response = HttpResponse(content_type="image/png")
+    # img.save(response, "PNG")
+    red = request.GET.get('red', '')
+    blue = request.GET.get('blue', '')
+    green = request.GET.get('green', '')
+
+    out_ds1 = out.ReadAsArray()[int(red)]
+    out_ds2 = out.ReadAsArray()[int(blue)]
+    out_ds3 = out.ReadAsArray()[int(green)]
+
+    im = numpy.dstack((out_ds1, out_ds2, out_ds3))
+    _, ar = cv2.imencode('.png', im)
+    response = HttpResponse(ar.tobytes(), content_type='image/png')
+
     return response
 
--- source.js
+++ source.js
@@ -1,5 +1,5 @@
 var mymap = L.map("map").setView([28.61, 77.2090], 7);
-var layer = L.tileLayer.wms("http://127.0.0.1:8000/",{
+var layer = L.tileLayer.wms("http://127.0.0.1:8000/?red=6&blue=4&green=3",{
 	format: "image/png"
 });
 
