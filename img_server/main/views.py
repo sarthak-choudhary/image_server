e17e465fae621cf663aabee1c1e829bc0a3c3614
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-06-21 17:08:05 +0530

    image decode

--- img_server/main/views.py
+++ img_server/main/views.py
@@ -4,6 +4,8 @@ from img_server.settings import BASE_DIR
 from PIL import Image
 from pyproj import Proj, transform
 from osgeo import gdal
+import matplotlib.pyplot as plt
+from PIL import Image
 import os
 import cv2
 import numpy
@@ -16,28 +18,34 @@ def index(request):
     inProj = Proj(init = 'epsg:3857')
     outProj = Proj(init = 'epsg:4326')
 
-    request_bbox = [float(t) for t in BBOX.split(',')]
-    request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
-    request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
-    print(request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1])
-    print()
-    out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]]) 
-
-    # out_ds = out.ReadAsArray()[0,...]
-    # img = Image.fromarray(out_ds).resize((256, 256), Image.NEAREST)
-    # response = HttpResponse(content_type="image/png")
-    # img.save(response, "PNG")
     red = request.GET.get('red', '')
     blue = request.GET.get('blue', '')
     green = request.GET.get('green', '')
 
-    out_ds1 = out.ReadAsArray()[int(red)]
-    out_ds2 = out.ReadAsArray()[int(blue)]
-    out_ds3 = out.ReadAsArray()[int(green)]
+    request_bbox = [float(t) for t in BBOX.split(',')]
+    request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
+    request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
+    
+    out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]], scaleParams = [[]]) 
 
-    im = numpy.dstack((out_ds1, out_ds2, out_ds3))
-    _, ar = cv2.imencode('.png', im)
-    response = HttpResponse(ar.tobytes(), content_type='image/png')
+    # out_ds1 = out.ReadAsArray()[int(red)]
+    # out_ds2 = out.ReadAsArray()[int(blue)]
+    # out_ds3 = out.ReadAsArray()[int(green)]
+    out_ds = out.ReadAsArray()
+
+
+    # im = numpy.array([out_ds1, out_ds3, out_ds2])
+    im = numpy.int16(numpy.dstack((out_ds[4,:,:], out_ds[3,:,:], out_ds[2,:,:])))
+    print(im.shape)
+    print()
+    pil_image = Image.fromarray(im)
+    # _, ar = cv2.imencode('.png', im)
+    # im = cv2.imdecode(ar, cv2.IMREAD_ANYCOLOR)
+
+    # plt.imshow(im)
+
+    response = HttpResponse(content_type='image/png')
+    pil_image.save(response, "PNG")
 
     return response
 
