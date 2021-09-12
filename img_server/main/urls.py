bee014b9d255e040c93cf6a4ab980421966f6514
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2021-09-12 13:49:58 +0530

    adds more remote sensing operations

--- img_server/main/urls.py
+++ img_server/main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add/", views.add, name="add"),
+    path("compute/<str:ops>", views.compute, name="compute"),
+    path("convolute/", views.convolute, name="convolute")
 ]
--- img_server/main/views.py
+++ img_server/main/views.py
@@ -13,7 +13,7 @@ import base64
 
 def index(request):
     BBOX = request.GET.get('bbox', 'BBOX')
-    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/out2019.tif"))
+    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/newsat.tif"))
     inProj = Proj(init = 'epsg:3857')
     outProj = Proj(init = 'epsg:4326')
 
@@ -25,18 +25,153 @@ def index(request):
     request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
     request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
     
-    out = gdal.Translate('',dataset, format='MEM', strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]], scaleParams = [[20, 3000]]) 
+    out = gdal.Translate('',dataset, format='MEM',outputType=gdal.GDT_Int16, strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]], scaleParams = [[]]) 
     out_ds = (out.ReadAsArray())
-    im = numpy.uint8(numpy.dstack((out_ds[4,:,:], out_ds[3,:,:], out_ds[2,:,:])))
-    im1 = numpy.int16(numpy.dstack((out_ds[2,:,:], out_ds[3,:,:], out_ds[4,:,:])))
+    # im = numpy.uint16(numpy.dstack((out_ds[4,:,:], out_ds[3,:,:], out_ds[2,:,:])))
+    im1 = numpy.dstack((out_ds[int(green),:,:], out_ds[int(blue),:,:], out_ds[int(red),:,:]))
 
-    print(im.shape)
+    # print(im.shape)
     print()
     _, ar = cv2.imencode('.png',im1)
     response = HttpResponse(ar.tobytes(),content_type="image/png;base64")
     return response
-    # pil_image = Image.fromarray(im)
-    # response = HttpResponse(content_type='image/png')
-    # pil_image.save(response, "PNG")
-    # return response
 
+
+def add(request):
+    BBOX = request.GET.get('bbox', 'BBOX')
+    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/newsat.tif"))
+    inProj = Proj(init = 'epsg:3857')
+    outProj = Proj(init = 'epsg:4326')
+
+    band1 = request.GET.get('band1', '')
+    band2 = request.GET.get('band2', '')
+
+    request_bbox = [float(t) for t in BBOX.split(',')]
+    request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
+    request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
+
+    out = gdal.Translate('',dataset, format='MEM',outputType=gdal.GDT_Int16, strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]], scaleParams = [[]])
+    out_ds = (out.ReadAsArray())
+
+    layer = out_ds[int(band1),:,:] + out_ds[int(band2),:,:]
+    im1 = numpy.dstack((layer, layer, layer))
+
+    _, ar = cv2.imencode('.png',im1)
+    response = HttpResponse(ar.tobytes(),content_type="image/png;base64")
+    return response
+
+
+def compute(request, ops):
+    BBOX = request.GET.get('bbox', 'BBOX')
+    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/newsat.tif"))
+    inProj = Proj(init = 'epsg:3857')
+    outProj = Proj(init = 'epsg:4326')
+
+    request_bbox = [float(t) for t in BBOX.split(',')]
+    request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
+    request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
+
+    out = gdal.Translate('',dataset, format='MEM',outputType=gdal.GDT_Int16, strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]], scaleParams = [[]])
+    out_ds = (out.ReadAsArray())
+
+    stack = []
+    openBrackets = []
+    for i in range(len(ops)):
+        if (ops[i] == '('):
+            openBrackets.append(len(stack))
+            stack.append(ops[i])
+        elif (ops[i] == ')'):
+            index = openBrackets.pop() + 1
+            reduceIndex = index - 1
+            layer = stack[index]
+            index += 1
+
+            while (index < len(stack)):
+                if (stack[index] == '+'):
+                    layer += stack[index + 1]
+                elif (stack[index] == '-'):
+                    layer -= stack[index + 1]
+                elif (stack[index] == '*'):
+                    layer *= stack[index + 1]
+                else:
+                    layer /= stack[index + 1]
+                    
+                index += 2
+
+            while (len(stack) != reduceIndex):
+                stack.pop()
+
+            stack.append(layer)
+        elif (ops[i] == '+' or ops[i] == '*' or ops[i] == '^' or ops[i] == '-'):
+            stack.append(ops[i])
+        else:
+            stack.append(out_ds[int(ops[i]),:,:].astype(float))
+
+    layer = stack.pop()
+
+    im1 = numpy.dstack((layer, layer, layer))
+
+    _, ar = cv2.imencode('.png',im1)
+    response = HttpResponse(ar.tobytes(),content_type="image/png;base64")
+    return response
+
+def convolve2D(image, kernel, padding=0, strides=1):
+    kernel = numpy.flipud(numpy.fliplr(kernel))
+
+    xKernShape = kernel.shape[0]
+    yKernShape = kernel.shape[1]
+    xImgShape = image.shape[0]
+    yImgShape = image.shape[1]
+
+    xOutput = int(((xImgShape - xKernShape + 2 * padding) / strides) + 1)
+    yOutput = int(((yImgShape - yKernShape + 2 * padding) / strides) + 1)
+    output = numpy.zeros((xOutput, yOutput))
+
+    if padding != 0:
+        imagePadded = numpy.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
+        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image[:,:,0]
+        print(imagePadded)
+    else:
+        imagePadded = image
+
+    for y in range(image.shape[1]):
+        if y > image.shape[1] - yKernShape:
+            break
+        if y % strides == 0:
+            for x in range(image.shape[0]):
+                if x > image.shape[0] - xKernShape:
+                    break
+                try:
+                    if x % strides == 0:
+                        output[x, y] = (kernel * imagePadded[x: x + xKernShape, y: y + yKernShape]).sum()
+                except:
+                    break
+
+    return output
+
+def convolute(request):
+    BBOX = request.GET.get('bbox', 'BBOX')
+    dataset = gdal.Open(os.path.join(BASE_DIR, "main/images/newsat.tif"))
+    inProj = Proj(init = 'epsg:3857')
+    outProj = Proj(init = 'epsg:4326')
+
+    band = request.GET.get('band', '')
+
+    request_bbox = [float(t) for t in BBOX.split(',')]
+    request_bbox[0], request_bbox[1] = transform(inProj, outProj, request_bbox[0], request_bbox[1])
+    request_bbox[2], request_bbox[3] = transform(inProj, outProj, request_bbox[2], request_bbox[3])
+
+    out = gdal.Translate('',dataset, format='MEM',outputType=gdal.GDT_Int16, strict=True,width=256, height=256, projWin = [request_bbox[0], request_bbox[3], request_bbox[2], request_bbox[1]], scaleParams = [[]])
+    out_ds = (out.ReadAsArray())
+
+    layer = out_ds[int(band),:,:]
+    im = numpy.dstack((layer, layer, layer))
+
+    kernel = numpy.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    output = convolve2D(im, kernel, padding=2)
+
+    im1 = numpy.dstack((output, output, output))
+
+    _, ar = cv2.imencode('.png',im1)
+    response = HttpResponse(ar.tobytes(),content_type="image/png;base64")
+    return response
